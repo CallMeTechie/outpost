0c462d395f20b658ff00a5e1c6c5262c390cb1b1
CallMeTechie <34693633+CallMeTechie@users.noreply.github.com>
2026-08-20 15:52:18 +0200

    chore: raise the version to 0.1.2

--- engine/src/proto/control_plane.h
+++ engine/src/proto/control_plane.h
@@ -6,7 +6,7 @@
 #include <pthread.h>
 #include <openssl/ssl.h>
 
-#define OUTPOST_ENGINE_VERSION "0.1.1"
+#define OUTPOST_ENGINE_VERSION "0.1.2"
 
 typedef struct outpost_control_plane {
     int sock_fd;
